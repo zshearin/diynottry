feaf98490df5f02ddd1b4e5e126cd9027fdc58df
Zach Shearin <zshearin617@gmail.com>
2023-04-17 21:14:00 -0400

    add tailwind and create rough draft of home page with chatgpt description
    * issue-6: add tailwind
    
    * change back
    
    * issue-2: added navigation skeleton, still need to make better looking
    
    * create basic home page layout - can fix later
    
    * issue-2: implement + tailwind
    
    * Try to fix the netlify cypress error
    
    * try 2 fix
    
    * remove netlify cypress plugin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: '**', component: NotFoundComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
