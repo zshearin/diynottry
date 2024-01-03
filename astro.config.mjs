01a8f940a368e3dc302c70e85e84ba05efb79f75
zshearin <zshearin617@gmail.com>
2024-01-02 20:46:44 -0500

    add netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,2 +1,8 @@
 import { defineConfig } from 'astro/config';
-export default defineConfig({});
+import netlify from '@astrojs/netlify';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  adapter: netlify()
+});
