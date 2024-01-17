aba7c99fe6cb7cf5c205a53ec1a826b9ff4dc9c4
zshearin <zshearin617@gmail.com>
2024-01-16 20:26:56 -0500

    add jenny of couch article and make mdx work

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 
 import tailwind from "@astrojs/tailwind";
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
-  integrations: [tailwind()]
+  integrations: [tailwind(), mdx()]
 });
