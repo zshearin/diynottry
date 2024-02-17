b67329f0f536ab447befe4b7021c78e02d5dd6cb
zshearin <zshearin617@gmail.com>
2024-02-17 17:24:13 -0500

    fix tailwind mdx stuff and some other minor things

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,20 @@ import netlify from '@astrojs/netlify';
 
 import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx'
+import addClasses from "rehype-add-classes";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
+  markdown: {
+    rehypePlugins: [[addClasses, {
+      h1: "text-4xl mb-2 font-normal",
+      h2: "text-2xl mb-2 font-normal",
+      h3: "text-xl  mb-1 font-normal",
+      h4: "text-lg font-normal",
+      h5: "font-normal",
+    }]]
+  },
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	safelist: ["text-4xl", "text-2xl", "text-xl", "text-lg", "font-normal"],
 	theme: {
 		extend: {},
 	},
